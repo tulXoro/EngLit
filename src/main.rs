24ddb0dbbd6b3a17510f99d1a1454d331251e48b
tulXoro <tulxoro@hotmail.com>
2023-08-27 23:36:26 -0700

    Complete lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use crate::lexer::Lexer;
+
+mod lexer;
+mod parser;
+
 fn main() {
-    println!("Hello, world!");
+    // Take args
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("Invalid number of args!");
+    }
+
+    let contents: String = std::fs::read_to_string(&args[2])
+        .expect("Could not find file!");
+
+    let lexer = Lexer{};
+
+    lexer.tokenize(contents);
+
 }
